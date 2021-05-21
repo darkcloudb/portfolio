7cd1b9b0b48c068a52cbd37201c89615584363b2
Billy Yip <birii.yip@gmail.com>
2021-05-21 12:36:18 -0700

    working on navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>My Portfolio Site</h1>
+      <Navbar />
     </div>
   );
 }
