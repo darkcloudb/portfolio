f5112962e85e648cc614b50d305a58221e6988e1
Billy Yip <birii.yip@gmail.com>
2021-05-23 07:59:18 -0700

    adjusted basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/darkcloudb">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
