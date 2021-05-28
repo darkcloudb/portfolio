413b74812ed90592e6b0a869382bdb3002350e45
Billy Yip <birii.yip@gmail.com>
2021-05-28 07:37:21 -0700

    fixing transition

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,38 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const pageTransition = {
+const contentAppear = {
   in: {
     opacity: 1,
-    y: 0,
   },
   out: {
     opacity: 0,
-    y: "-100%",
   },
 };
 
 function Header() {
   return (
-    <motion.div
-      className="header-wrapper"
-      initial="out"
-      animate="in"
-      exit="out"
-      variants={pageTransition}
-    >
+    <motion.div className="header-wrapper">
       <div className="main-info">
-        <h1>About Me </h1>
-        <h3>
+        <motion.h1
+          initial="out"
+          animate="in"
+          exit="out"
+          variants={contentAppear}
+        >
+          About Me{" "}
+        </motion.h1>
+        <motion.h3
+          initial="out"
+          animate="in"
+          exit="out"
+          variants={contentAppear}
+        >
           Hi, my name is Billy Yip and I am currently located in the San
           Francisco Bay Area. My career goal is to be an Software Engineer, Web
           Developer, or Game Designer / Programmer to further develop my skills
           while playing a role in the innovation of programs and/or video games.
-        </h3>
+        </motion.h3>
       </div>
     </motion.div>
   );
